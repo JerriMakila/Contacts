850fa29d2f3cc33c332ac433a69a2a72f81fa5c8
Jerri Mäkilä <jerri.makila@gmail.com>
2021-02-15 17:21:48 +0200

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import * as Contacts from 'expo-contacts';
 
 export default function App() {
+
+  const [contacts, setContacts] = useState([]);
+
+  const getContacts = async () => {
+    const {status} = await Contacts.requestPermissionsAsync();
+
+    if(status === 'granted'){
+      const {data} = await Contacts.getContactsAsync({
+        fields : [Contacts.Fields.PhoneNumbers]
+      });
+
+      let newData = [];
+
+      data.map(contact => {
+        if(contact.hasOwnProperty('phoneNumbers')){
+          newData.push(contact);
+        }
+      });
+
+      if(newData.length > 0){
+        setContacts(newData);
+      }
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {console.log(contacts)}
+      {contacts.length > 0 ? (
+         <FlatList
+           data={contacts}
+           keyExtractor={item => item.name}
+           renderItem={({item}) =>
+              <View>
+               <Text style={{fontSize: 20}}>{item.name} {item.phoneNumbers[0].number}</Text>
+             </View>
+            }
+             
+            
+            />
+      ) : (
+        <Text>Waiting</Text>
+      )}
+      
+      <Button title='PRESS' onPress={getContacts} />
+      <StatusBar hidden={true} />
     </View>
   );
 }
@@ -16,6 +59,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 30
   },
+
+  listItem: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  }
 });
